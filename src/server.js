844888a05786a09093ac624130669a8427ac8001
Gilbert Kim <45444133+gbertkim@users.noreply.github.com>
2021-08-23 11:11:48 -0700

    Add files via upload
    Updated SSL certificates and dependencies to run on Heroku. Node 14x.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
+const parse = require("pg-connection-string").parse;
+const pgconfig = parse(process.env.DATABASE_URL);
+pgconfig.ssl = { rejectUnauthorized: false };
 
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL,
+  connection: pgconfig,
 })
 
 app.set('db', db)
